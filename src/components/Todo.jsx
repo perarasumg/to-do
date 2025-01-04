4563d922aa9cc1e0afa870c7536b44f28c67a79f
perarasumg <perarasumg@gmail.com>
2025-01-04 18:30:01 +0530

    changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -15,39 +15,38 @@ export const Todo = () => {
       isComplete: true,
     },
   ]);
-    const inputRef= useRef()
-// add task
-    const addTask = () => {
-      const inputText = inputRef.current.value.trim();
-      if (inputText === "") 
-       {
-        return null;
-       }
-       const newTask = {
-        id: Date.now(),
-        text: inputText,
-        isComplete: false,
-       }
-        SetTodoList([...todoList, newTask]);
-        inputRef.current.value="";
+  const inputRef = useRef();
+  // add task
+  const addTask = () => {
+    const inputText = inputRef.current.value.trim();
+    if (inputText === "") {
+      return null;
+    }
+    const newTask = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
     };
+    SetTodoList([...todoList, newTask]);
+    inputRef.current.value = "";
+  };
 
-    const completeStatus = (id) => {
-      SetTodoList((prev)=> {
-        return prev.map((todo) => {
-          if (todo.id === id) {
-            return { ...todo, isComplete: !todo.isComplete };
-          }
-          return todo;
-        });
+  const completeStatus = (id) => {
+    SetTodoList((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isComplete: !todo.isComplete };
+        }
+        return todo;
       });
-    };
+    });
+  };
 
-    const removeElement = (id) => {
-      SetTodoList((prev) => {
-        return prev.filter((todo) => todo.id !== id);
-      });
-    };
+  const removeElement = (id) => {
+    SetTodoList((prev) => {
+      return prev.filter((todo) => todo.id !== id);
+    });
+  };
 
   return (
     <>
@@ -64,7 +63,10 @@ export const Todo = () => {
               className="px-3 py-4 w-full text-sm border focus:outline-none focus:border-blue-400"
             />
           </div>
-          <button onClick={addTask} className="py-3 px-4 bg-blue-600 text-white hover:bg-blue-700 font-medium rounded-sm border-none ">
+          <button
+            onClick={addTask}
+            className="py-3 px-4 bg-blue-600 text-white hover:bg-blue-700 font-medium rounded-sm border-none "
+          >
             Add Task
           </button>
         </div>
@@ -74,7 +76,7 @@ export const Todo = () => {
         <fieldset>
           <legend className="text-green-500 font-medium">List of task</legend>
           {todoList.length !== 0 ? (
-            todoList.map(( todo,  index ) =>{
+            todoList.map((todo, index) => {
               return (
                 <TodoItem
                   text={todo.text}
@@ -94,9 +96,3 @@ export const Todo = () => {
     </>
   );
 };
-
-
-
-
-
-    
